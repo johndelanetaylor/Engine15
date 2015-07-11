fa09519afc4b922acedf5a802f0d20a61bba663a
sethmcleod <me@sethmcleod.com>
2015-07-11 18:12:23 -0400

    Slideshow Placeholder

--- index.php
+++ index.php
@@ -10,22 +10,8 @@
 	
 <?php include($_SERVER['DOCUMENT_ROOT']."/snippets/nav.php"); ?>
     
-<!-- Slider Placeholder
-<div style="text-align: center; width: 100%; margin: 20px 0 40px;">
-    <img src="img/slider.jpg" style="margin:auto; max-width: 100%;" />
-</div> -->
-    
-<!-- Slider -->
-<!-- <div class="home-slider">
-    <div class="slide"><img src="img/slide-1.jpg"></div>
-    <div class="slide"><img src="img/slide-2.jpg"></div>
-    <div class="slide"><img src="img/slide-3.jpg"></div>
-</div>-->
-    
 <!-- Slider Placeholder -->
-<section class="cover">
-    <img src="http://placehold.it/1440x560">
-</section>
+<section class="cover" style="background-image: url('img/slide-1.jpg');"></section>
     
 <!-- Locations -->
 <?php include($_SERVER['DOCUMENT_ROOT']."/snippets/locations.php"); ?>
