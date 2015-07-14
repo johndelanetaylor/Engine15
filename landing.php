0c7d5cfe083e014bc0066e2515b9da58500b7243
johndelanetaylor <johndelanetaylor@gmail.com>
2015-07-14 17:28:03 -0400

    built out landing page html and css. Still needs vertical centering.

---
+++ landing.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang='en'>
+<?php $pageTitle = 'Landing'; ?>
+    
+<head>
+	<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/header.php"); ?>
+</head>
+
+<body>
+
+    <section class="landing">
+        <div class="container">
+            <div class="row">
+                <div class="offset-by-two eight columns">
+                    <div class="logo-lg">
+                        <img src="img/engine-15-logo-large.png" />
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="offset-by-one four columns">
+                    <h1>Over 21</h1>
+                    <h5>I need a beer or 3</h5>
+                    <button>I am over 21</button>
+                </div>
+                <div class="two columns">
+                    <h4>or</h4>
+                </div>
+                <div class="four columns">
+                    <h1>Under 21</h1>
+                    <h5>Get me out of here</h5>
+                    <button>I am under 21</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+</body>
+</html>
