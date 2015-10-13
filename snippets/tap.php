8c49949d308bf41b9c5b15814b814cc279c4af9c
johndelanetaylor <johndelanetaylor@gmail.com>
2015-10-13 10:55:41 -0400

    removed beer ratings

--- snippets/tap.php
+++ snippets/tap.php
@@ -8,9 +8,9 @@
         <div class="row">
             <div class="twelve columns sm-pad" style="padding-bottom: 0;">
                 <script type='text/javascript' src='http://www.whatsontap.buildabeer.org/Websites/TapInfo_USAFL00047.js'></script>
-                
-                <script type='text/javascript'>sWOTAItemType='Tap';bWOTAShowRatings=true;iWOTAStyle=1;iWOTAFirst=0;iWOTALast=0;</script>
-                
+
+                <script type='text/javascript'>sWOTAItemType='Tap';bWOTAShowRatings=false;iWOTAStyle=1;iWOTAFirst=0;iWOTALast=0;</script>
+
                 <script type='text/javascript' src='http://www.whatsontap.buildabeer.org/Websites/WOTA_Show.js'></script>
             </div>
         </div>
